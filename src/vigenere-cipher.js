a627daea9aba96a142c791d0e21c9e6a160b5726
Myra Atabaeva <atabaeva.pgr@gmail.com>
2025-01-07 04:30:47 +0500

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabetSize = 26;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (/[A-Z]/.test(message[i])) {
+        const m = message.charCodeAt(i);
+        const k = key.charCodeAt(keyIndex % key.length);
+        const c = (m + k) % this.alphabetSize;
+        result += String.fromCharCode(c + 65);
+
+        keyIndex++;
+      } else {
+        result += message[i];
+      }
+    }
+      return this.isDirect ? result : result.split("").reverse().join("");
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage|| !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    const message = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = "";
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (/[A-Z]/.test(message[i])) {
+        const c = message.charCodeAt(i);
+        const k = key.charCodeAt(keyIndex % key.length);
+        const m = (c - k + this.alphabetSize) % this.alphabetSize;
+        result += String.fromCharCode(m + 65);
+
+        keyIndex++;
+      } else {
+        result += message[i];
+      }
+    }
+    return this.isDirect ? result : result.split("").reverse().join("");
   }
 }
 
